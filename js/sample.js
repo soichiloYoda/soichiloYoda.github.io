776dc833222c77fa9f313081418908dd5b9253d8
soichiloYoda <soichilo.yoda@gmail.com>
2017-07-18 15:04:15 +0900

    リスト表示の画像修正

--- js/sample.js
+++ js/sample.js
@@ -3,14 +3,14 @@ $(function(){
 		var sH = window.innerHeight;
 		aspectRatio = sW / sH;
 		if(aspectRatio >= 1){
-			//横長画像の場合 divのheightに数値を合わせる
+			//横長画面
 			$('#music img').css('width','100%');
 			$('#photo img').css('width','100%');
 			$('#web img').css('width','100%');
 		}else{
-			//縦長画像の場合 divのwidthに数値を合わせる
-			$('#music img').css('height','100%');
-			$('#photo img').css('height','100%');
-			$('#web img').css('height','100%');
+			//縦長画面
+			$('#music img').css('width','100%');
+			$('#photo img').css('width','100%');
+			$('#web img').css('width','100%');
 		}
 });
